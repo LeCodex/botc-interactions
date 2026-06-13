f6a6e162385a734869fc3cfa2509e1a201ecdf00
LeCodex <40335983+LeCodex@users.noreply.github.com>
2026-06-13 16:09:09 +0200

    feat: update interactions, make the roles be in alphabetical order in categories, and show diffs on generation

--- generate_json_files.py
+++ generate_json_files.py
@@ -1,18 +1,18 @@
 import csv
 import json
 import requests
+from typing import Any
 
 def extract_messages(row):
     messages = {}
 
     for k, v in row.items():
-        print("  " + k)
         if v == "" or v == ".": continue
         messages[k] = parse_interaction(v)
 
     return messages
 
-categories = {"??": "great", "!!": "conflict", "?": "good", "!": "warning", "@": "group" }
+categories = {"??": "great", "!!": "conflict", "?": "good", "!": "warning", "@": "group"}
 def parse_interaction(input: str):
     # Separate the string in the different messages
     messages = {}
@@ -58,6 +58,47 @@ def extract_extras(row: list[str]):
         "characters": [x for x in row if len(x) > 0]
     }
 
+def alphabetical_path(path: str | None, k: str):
+    return (
+        k if path is None else (
+            f"{path}.{k}" if path < k else f"{k}.{path}"
+        ) if "." not in path else
+        f"{path}.{k}"
+    )
+
+def recursive_diff(a: dict, b: dict, path: str | None = None, diffs: dict[str, tuple[Any | None, Any | None]] = {}):
+    for k, v in a.items():
+        full_path = alphabetical_path(path, k)
+        if k not in b:
+            if type(v) == dict:
+                recursive_diff(v, {}, full_path)
+            else:
+                diffs[full_path] = (v, diffs[full_path][1] if full_path in diffs else None)
+            continue
+
+        other = b[k]
+        if type(v) == dict and type(other) == dict:
+            recursive_diff(v, other, full_path)
+        elif v != other:
+            diffs[full_path] = (v, other)
+
+    for k, v in b.items():
+        full_path = alphabetical_path(path, k)
+        if k not in a:
+            if type(v) == dict:
+                recursive_diff({}, v, full_path)
+            else:
+                diffs[full_path] = (diffs[full_path][0] if full_path in diffs else None, v)
+            continue
+
+    if path is None:
+        for subpath, (minus, plus) in diffs.items():
+            if minus == plus: continue
+            print(f"\033[1m{subpath}:\033[00m")
+            if minus: print(f"\t\033[91m- {minus}\033[00m")
+            if plus: print(f"\t\033[92m+ {plus}\033[00m")
+
+
 outputs = {}
 base_url = "https://docs.google.com/spreadsheets/d/1KBF5DurN0zq8eSLuh3u2Lg2w7e6Fr7N4B5qzc-wRO4U/gviz/tq?tqx=out:csv"
 for tab in ["Matchups", "Hermit"]:
@@ -66,8 +107,17 @@ for tab in ["Matchups", "Hermit"]:
     output = {}
     for row in reader:
         key = row.pop("")
-        print(key)
         output[key] = extract_messages(row)
+
+    try:
+        json_filename = f"./{tab.lower()}.json"
+        with open(json_filename, "r") as file:
+            existing = json.load(file)
+            recursive_diff(existing, output)
+        input("Press Enter to continue")
+    except:
+        pass
+
     outputs[tab] = output
 
 for tab in ["Groups"]:
